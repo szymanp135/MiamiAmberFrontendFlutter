e636a4dc848e035bcea810f6e066d4d29e6c0e3a
pawel <szymanski.pawel.100@gmail.com>
2026-03-11 19:37:16 +0100

    changed default theme to dark

--- lib/constants.dart
+++ lib/constants.dart
@@ -4,6 +4,6 @@ const kMiamiAmberColor = Color(0xFFFFC000);
 const kLightBgColor = Colors.white;
 const kDarkBgColor = Color(0xFF1E1E1E);
 
-const String versionText = "v1.260311.2";
+const String versionText = "v1.260311.3";
 
 const TextStyle pageTitleTextStyle = TextStyle(fontWeight: FontWeight.bold);
--- lib/providers/theme_provider.dart
+++ lib/providers/theme_provider.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class ThemeProvider with ChangeNotifier {
-  ThemeMode _themeMode = ThemeMode.light;
+  ThemeMode _themeMode = ThemeMode.dark;
 
   ThemeMode get themeMode => _themeMode;
   bool get isDarkMode => _themeMode == ThemeMode.dark;
